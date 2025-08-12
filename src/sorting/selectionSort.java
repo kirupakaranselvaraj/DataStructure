e244fcffc78374f3f7693840a19b4598f372cc30
kirupa <kirupakaran.s151205@gmail.com>
2025-08-12 15:04:55 +0530

    sorting

---
+++ src/sorting/selectionSort.java
@@ -0,0 +1,31 @@
+package sorting;
+import java.util.Scanner;
+public class selectionSort {
+    public static void selectionSort(int[] arr) {
+        int minIndex;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selectionSort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
